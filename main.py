4191538097dfadc92e8d3014412d91cced32697f
frappic <110016571+frappic@users.noreply.github.com>
2022-07-26 17:34:57 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import string
+import random
+from tkinter import N
+import pyperclip as pc
+
+        ####ENTER PASSWORD CHARACTER LEGNTH 
+length = int(input("Enter password length: \n"
+"\n"
+"\n"
+
+"16 or more is highly recommended!!-: "))
+
+        ####LABEL OR TITLE FOR PASSWORD
+        ####TO REMEMBER WHAT YOU USED IT FOR...
+label = input('Enter a label for this password to be stored with...'  
+"\n"
+"\n")
+
+        ####ALPHA AND NUMERIC LIBRARY 
+        ####AS WELL AS SYMBOLS 
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+all = string.ascii_letters + string.digits + string.punctuation
+        ####RANDOMIZE STRING
+temp = random.sample(all, length)
+
+        ####COMBINIG LENGTH INPUT W/ RANDOM STRING
+password = "".join(random.sample(all, length))
+
+        ####COPY TO CLIPBOARD SECURLEY
+pc.copy(password)
+
+        ####OPTIONAL BUT CONVENIENT
+        ####APPEND TEXT FILE TO STORE PASSWORD W/ LABEL 
+with open('password.txt', 'a') as f:
+
+    f.write('\n')
+    f.write(label)
+    pc.paste()
+    f.write(' -- ')
+    f.write(password)
+    f.close()
+    print("Your password has been copied to the clipboard and saved to a text file in the same folder as this program. :)  ")
+
+    exit = input("Press 'enter' to exit this program.")
+
+    ####
+
+
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+This is a quick and easy tool to help you generate passwords strong enough to provide better protection against bot brute force attacks. 
+***This method, however, does not GUARANTEE that a password will have enough entropy to be completely safe.***
+
+Its important for your passwords to contain both UPPERCASE and lowercase letters, numbers, and as well as symbols such as "!@#$%^&*()_+" .
+Brute force attacks are prevelent and often have success with simple, short passwords. To give you a better understanding, 
+check this out: "https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt"
+
+python
